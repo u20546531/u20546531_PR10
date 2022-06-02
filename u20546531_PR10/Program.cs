a457f884ed1b5da6a29d27bbbbb456868db963ed
Mmaphuti Mpai(RTCW) <Mmaphuti.Mpai@ekurhuleni.gov.za>
2022-06-02 10:59:18 +0200

    Add project files.

---
+++ u20546531_PR10/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace u20546531_PR10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
